4b043e05fb734a61a6a5e4eeaaff5492af3692f8
drewdoesdev <ataylor2247@gmail.com>
2018-02-14 17:58:50 -0600

    Spelling errors corrected

--- src/components/chapter_three.js
+++ src/components/chapter_three.js
@@ -4,7 +4,7 @@ import imageTwo from "../img/halloween.jpg";
 import imageThree from "../img/kts-graph.jpg";
 
 const ChapterThree = (props) => {
-  const paragraph = "Turns out it’s no easy feat to endlessly have amazing times with a stunningly gorgeos woman, who you seem to have a cosmic levels of chemistry with, and -not- fall head over heels for her.  And before he knew it, Sawyer found himself being that old cliche of of the guy hopelessly infatuated with his best friend.  For months he kept these feelings to himself, knowing that fessing up to Nicole would probably mean the end of a relationship that meant everything to him.";
+  const paragraph = "Turns out it’s no easy feat to endlessly have amazing times with a stunningly gorgeous woman, who you seem to have a cosmic levels of chemistry with, and -not- fall head over heels for her.  And before he knew it, Sawyer found himself being that old cliche of of the guy hopelessly infatuated with his best friend.  For months he kept these feelings to himself, knowing that fessing up to Nicole would probably mean the end of a relationship that meant everything to him.";
   return (
     <div className="chapter chapter-two">
          <div className="chap-text">
